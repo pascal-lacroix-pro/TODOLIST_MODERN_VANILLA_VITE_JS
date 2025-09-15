8a84975845b8f3ad6a0813d771cc996682e81f5a
Pascal Lacroix <p.lacroix@promotion-sociale.be>
2025-09-15 14:06:02 +0200

    Intégration des templates

--- src/components/todo/template.js
+++ src/components/todo/template.js
@@ -1,5 +1,16 @@
 import "./styles.css";
 
 export default function (todo) {
-  return `<li class="todo">${todo.content}</li>`;
+  return `
+    <li data-id="1" class="${todo.completed ? "completed" : ""}">
+            <div class="view">
+              <input class="toggle" type="checkbox" ${
+                todo.completed ? "checked" : ""
+              } />
+              <label>${todo.content}</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" value="${todo.content}" />
+          </li>
+  `;
 }
